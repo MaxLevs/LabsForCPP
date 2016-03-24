1fc259fa08ddd7dba51a66c59f6ecf67c5d9afdf
MaxLevs <maxevs@mail.ru>
2016-03-24 18:41:57 +1000

    Добавил ввывод ошибок в debug.txt

--- sem2/compilate.sh
+++ sem2/compilate.sh
@@ -2,5 +2,5 @@
 outPath='results'
 inFiles=$(ls -a | grep .cpp)
 for i in $inFiles; do echo $outPath"/"${i//.cpp/.out};
-	g++ -std=c++11 -Wall $i -o $outPath"/"${i//.cpp/.out}
+	g++ -std=c++11 -Wall $i -o $outPath"/"${i//.cpp/.out} 2> debug.txt
 done
