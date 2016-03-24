487a2ff013dccc7116a26e0e0b7be1f494271fb1
MaxLevs <maxevs@mail.ru>
2016-03-24 19:17:43 +1000

    вычисление значения параметрической функции

--- sem2/Task2.1.cpp
+++ sem2/Task2.1.cpp
@@ -3,9 +3,16 @@
 using namespace std;
 int main() {
 	setlocale(LC_ALL, "Russian");
-	double x, y, R;
-	cout << "Введите коорднаты точки (скачала X, а потом Y) и радиус окружности области:\n";
-	cin >> x >> y >> R;
-	if (x*x + y*y <= R*R && (((y >= (x - 1)*(x - 1)) && (x >= 0)) || ((x < 0) && (y <= 0)))) cout << "Попадение!\n"; else cout << "Промах!\n";
-	system("Pause");
-};
+	float x, y;
+	system("clear");
+	cout <<"Введите значение аргумента: ";
+	cin >> x;
+	if (x<=-2) y=0;
+	else if (x<-1) y=-x-2;
+	else if (x<1) y=x;
+	else if (x<2) y=-x+2;
+	else y=0;
+	cout << "При x, равном " << x << ", функция y(x)=" << y << "\n";
+	system("echo; echo;");
+	return 0;
+}
