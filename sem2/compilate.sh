5f802b86f72e460aaa606b77f5ace1e528ef0665
MaxLevs <maxevs@mail.ru>
2016-03-24 21:55:25 +1000

    Исключения для 2.3

--- sem2/compilate.sh
+++ sem2/compilate.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 outPath='results'
+j=1
 inFiles=$(ls -a | grep .cpp)
 for i in $inFiles; do echo $outPath"/"${i//.cpp/.out};
-	g++ -std=c++11 -Wall $i -o $outPath"/"${i//.cpp/.out} 2> debug.txt
+	if [ "$j" -eq "4" ]; then addict="-l curses"; else addict=""; fi
+	g++ -std=c++11 -Wall $i -o $outPath"/"${i//.cpp/.out} $addict 2> debug.txt
+	j=$[$j+1]
 done
