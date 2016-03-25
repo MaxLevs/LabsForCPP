267673983aa201f90925fe1563605db4c1303aeb
MaxLevs <maxevs@mail.ru>
2016-03-25 11:24:05 +1000

    Линкуем для ncurses

--- sem2/compilate.sh
+++ sem2/compilate.sh
@@ -3,7 +3,7 @@ outPath='results'
 j=1
 inFiles=$(ls -a | grep .cpp)
 for i in $inFiles; do echo $outPath"/"${i//.cpp/.out};
-	if [ "$j" -eq "4" ]; then addict="-l curses"; else addict=""; fi
+	if [ "$j" -eq "4" ]; then addict="-lncurses"; else addict=""; fi
 	g++ -std=c++11 -Wall $i -o $outPath"/"${i//.cpp/.out} $addict 2> debug.txt
 	j=$[$j+1]
 done
