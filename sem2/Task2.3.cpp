b001a4187489d78fce92b9e23a2efea1765b50d7
MaxLevs <maxevs@mail.ru>
2016-03-25 12:33:50 +1000

    Рабочая версия, но с крокозябрами

--- sem2/Task2.3.cpp
+++ sem2/Task2.3.cpp
@@ -1,24 +1,24 @@
 #include <cstdio>
-#include <curses.h> //cd /usr/include/c++/4.8.4
+#include <ncurses.h> //cd /usr/include/c++/4.8.4
 #include <iostream>
 
 using namespace std;
 
 int main() {
-	printf("\nНажмите одну из курсорных клавиш:\n");
-	system("sleep 1s");
 	initscr();
+	printw("Нажмите одну из курсорных клавиш:\n");
 	keypad(stdscr, true);	//Включаем режим чтения функциональных клавиш
 	noecho();				//Выключаем отображение вводимых символов, нужно для getch()
 	halfdelay(100);			//Устанавливаем ограничение по времени ожидания getch() в 10 сек
 	int key = getch();
-	endwin();
-	switch ( key ) {
-		case KEY_UP: printf("Стрелка вверх\n"); break;
-		case KEY_LEFT: printf("Стрелка влево\n"); break;
-		case KEY_RIGHT: printf("Стрелка вправо\n"); break;
-		case KEY_DOWN: printf("Стрелка вниз\n"); break;
+	switch (key) {
+		case KEY_UP: printw("Стрелка вверх!\n"); break;
+		case KEY_LEFT: printw("Стрелка влево!\n"); break;
+		case KEY_RIGHT: printw("Стрелка вправо!\n"); break;
+		case KEY_DOWN: printw("Стрелка вниз!\n"); break;
 		default: printf("Это что-то не то!\n");
 	}
+	key = getch();
+	endwin();
 	return 0;
 }
