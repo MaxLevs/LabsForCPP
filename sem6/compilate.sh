95c0d1c3e97e0e6df72a7f7e48803e5a498de66a
MaxLevs <maxevs@mail.ru>
2016-05-04 17:35:08 +1000

    Добавлен Автокомпилятор

---
+++ sem6/compilate.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+outPath='results'
+inFiles=$(ls -a | grep .cpp)
+for i in $inFiles; do echo $outPath"/"${i//.cpp/.out};
+	g++ -std=c++11 -Wall $i -o $outPath"/"${i//.cpp/.out} 2> debug.txt
+done
