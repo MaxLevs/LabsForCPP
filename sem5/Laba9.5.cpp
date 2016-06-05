4804a56bf63f457e918c5efee6bae59744c031b3
MaxLevs <maxevs@mail.ru>
2016-06-05 11:56:06 +1000

    Задание 5

--- sem5/Laba9.5.cpp
+++ sem5/Laba9.5.cpp
@@ -7,7 +7,10 @@ int main() {
 	ifstream fin( "text1.txt" );
 	if ( !fin ) { cout << "Ошибка открытия файла." << endl; return 1; }
 
-	slu
+	long slu = 0;
+
+	cout << "Кол-во слов: ";
+	cin >> slu;
 
 	fin.seekg( 0, ios::end );
 	long len = fin.tellg();
@@ -15,14 +18,20 @@ int main() {
 	fin.seekg( 0, ios::beg );
 	fin.read( buf, len );
 
-	buf[len] = '\0'; long n = 0, i = 0; sl = 0;
-	while( buf[i] ) {
-		if isspace(buf[i]) sl++;
-		if( buf[i] == '?' || buf[i] == '.' || buf[i] == '!') {
-			if (sl+1 == slu) {
+	buf[len] = '\0'; long n = 0, i = 0, sl = 0;
+	while( buf[i] != '\0' ) {
+		if (isspace(buf[i])) {
+			sl++;
+		}
+		
+		if (buf[i] == '?' || buf[i] == '.' || buf[i] == '!') {
+			if (sl+1 == slu){
 				for ( int j = n; j <= i; j++ ) cout << buf[j];
-				n = i + 1;
+				cout << endl;
 			}
+			while (isspace(buf[i+1])) i++; //Считаем до начала след предложения.
+			n = i+1; //Убираем единственный пробельный символ.
+			sl = 0; //Зануление счетчика слов.
 		}
 		i++;
 	}
